31d0c700486ae14727c39754450baa94c0200c83
Jooyong Yi <jooyong@unist.ac.kr>
2020-12-01 10:26:07 +0900

    compile.sh should work now in Windows

--- fuzz/compile.sh
+++ fuzz/compile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#rm examples/simple/*.class
+rm -f examples/simple/*.class
 
-# give your own ROOT dir
-ROOT=/SE_Assignment4/fl-assignment
-FUZZ_TARGET=$ROOT/fuzz/target
-# you can change the junit version
-javac -cp $FUZZ_TARGET/classes:$FUZZ_TARGET/dependency/junit-4.13.1.jar:$FUZZ_TARGET/dependency/junit-quickcheck-core-0.8.jar:. examples/simple/ExampleClassTest.java
-javac -cp $FUZZ_TARGET/classes:$FUZZ_TARGET/dependency/junit-4.13.1.jar:$FUZZ_TARGET/dependency/junit-quickcheck-core-0.8.jar:. examples/simple/ExampleClass.java
+javac -cp target/classes examples/simple/ExampleClass.java
+
+# If you use Linux or mac, use ":" instead of "\;".
+javac -cp target/classes\;target/dependency/junit-4.13.1.jar\;target/dependency/junit-quickcheck-core-0.8.jar\;. examples/simple/ExampleClassTest.java
